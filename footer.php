f0f3b2f97d260baeb3e31c9a6c2778e7c580fa31
Hitesh Save <savehitesh@gmail.com>
2022-03-09 14:56:43 +0530

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 <?PHP
 
-echo "<br><b><p align=center>Copyright &copy; 2020-" . date("Y") . " <a href='$company_website' target='_blank'<b>$company_name<b></a></p>";
+echo "<br><b><p align=center>Copyright &copy; 2022-" . date("Y") . " <a href='$company_website' target='_blank'<b>$company_name<b></a></p>";
 // echo "<font size=1>Design and code by Dan Medhurst 2017</font><br>";
 // echo "<font size=1>Icons made by <a href='http://www.flaticon.com/authors/icon-works'>Icon Works</a> from <a href='http://www.flaticon.com'>www.flaticon.com</a></font>";
 ?>
